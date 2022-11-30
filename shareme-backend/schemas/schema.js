7cadb8d297217a78ab84502ce825b05af8481f60
Daniel Batista <dani_batista20@hotmail.com>
2022-11-30 08:04:58 +0000

    add: schema - postedBy

--- shareme-backend/schemas/schema.js
+++ shareme-backend/schemas/schema.js
@@ -5,6 +5,10 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 // user schema
 import user from './user'
+import pin from './pin'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,6 +17,10 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    user
+    user,
+    pin,
+    comment,
+    postedBy,
+    save
   ])
 })
